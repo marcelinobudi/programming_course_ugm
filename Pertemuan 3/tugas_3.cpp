046d4a1a795e8358dd635d0f66f0c4cc9e03116f
marcelinobudi <marcelinobprakasya@gmail.com>
2025-09-06 20:23:51 +0700

    menambahkan tugas 3: menghitung persentase lemak tubuh

---
+++ Pertemuan 3/tugas_3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    char sex;
+    double p_a1, p_a2, p_a3, p_a4, p_a5;
+    double l_a1, l_a2;
+    double b;
+    float body_fat;
+    float fat_percentage;
+
+    cout << "Anda laki-laki atau perempuan? (l/p): ";
+
+    switch (sex)
+    {
+    case 'l':
+        /* code */
+        break;
+    
+    case 'p':
+        break;
+
+    default:
+        cout << "Input yg anda masukkan salah!" << endl;
+        break;
+    }
+
+    
+    return 0;
+}
