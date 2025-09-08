b0950033081149150a7b1576f536725740ecf47b
marcelinobudi <marcelinobprakasya@gmail.com>
2025-09-08 11:06:18 +0700

    Menambahkan konversi dari kg ke lb dan dari cm ke inches

--- Pertemuan 3/tugas_3.cpp
+++ Pertemuan 3/tugas_3.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+double convert_kg_to_lb(double mass) {
+    return mass * (1/0.453592);
+}
+
+double convert_cm_to_inch(double length) {
+    return length * (1/2.54);
+}
+
 int main(int argc, char const *argv[])
 {
     char sex;
@@ -15,44 +23,52 @@ int main(int argc, char const *argv[])
     cout << "Anda laki-laki atau perempuan? (l/p): ";
     cin >> sex;
 
-    cout << "Masukkan berat badan anda: ";
+    cout << "Masukkan berat badan anda (kg): ";
     cin >> weight;
+    weight = convert_kg_to_lb(weight);
 
     switch (sex)
     {
-    case 'l':
-        
-        cout << "Masukkan panjang lingkar pinggang anda: ";
-        cin >> waist_size;
+        case 'l':
+            
+            cout << "Masukkan panjang lingkar pinggang anda (cm): ";
+            cin >> waist_size;
+            waist_size = convert_cm_to_inch(waist_size);
 
-        a1 = (weight * 1.082 ) / + 94.2;
-        a2 = waist_size * 4.15;
-        
-        b = a1 - a2;
-        break;
-    
-    case 'p':
-        cout << "Masukkan panjang lingkar pergelangan tangan anda: ";
-        cin >> wrist_size;
-        cout << "Masukkan panjang lingkar pinggang anda: ";
-        cin >> waist_size;
-        cout << "Masukkan panjang lingkar pinggul anda: ";
-        cin >> hip_size;
-        cout << "Masukkan panjang lingkar lengan bawah anda: ";
-        cin >> lower_arm_size;
+            a1 = (weight * 1.082 ) + 94.2;
+            a2 = waist_size * 4.15;
+            
+            b = a1 - a2;
 
-        a1 = (weight * 0.732) + 8.987;
-        a2 = wrist_size / 3.140;
-        a3 = waist_size * 0.157;
-        a4 = hip_size * 0.249;
-        a5 = lower_arm_size * 0.434;
+            cout << a1 << " " << a2 << " " << b;
+            break;
         
-        b = a1 + a2 - a3 -a4  + a5;
-        break;
+        case 'p':
+            cout << "Masukkan panjang lingkar pergelangan tangan anda: ";
+            cin >> wrist_size;
+            wrist_size = convert_cm_to_inch(wrist_size);
+            cout << "Masukkan panjang lingkar pinggang anda: ";
+            cin >> waist_size;
+            waist_size = convert_cm_to_inch(waist_size);
+            cout << "Masukkan panjang lingkar pinggul anda: ";
+            cin >> hip_size;
+            hip_size = convert_cm_to_inch(hip_size);
+            cout << "Masukkan panjang lingkar lengan bawah anda: ";
+            cin >> lower_arm_size;
+            lower_arm_size = convert_cm_to_inch(lower_arm_size);
 
-    default:
-        cout << "Input yg anda masukkan salah!" << endl;
-        return 0;
+            a1 = (weight * 0.732) + 8.987;
+            a2 = wrist_size / 3.140;
+            a3 = waist_size * 0.157;
+            a4 = hip_size * 0.249;
+            a5 = lower_arm_size * 0.434;
+            
+            b = a1 + a2 - a3 -a4  + a5;
+            break;
+
+        default:
+            cout << "Input yg anda masukkan salah!" << endl;
+            return 0;
     }
 
     body_fat =  weight - b;
